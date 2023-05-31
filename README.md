8bc4145ac09259be1f79d776873686db70f14e28
Manuel Leduc <manuel.leduc@xwiki.com>
2023-05-31 16:52:21 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Webpack Demo
+
+## Build for production
+
+```bash
+npx webpack --mode=production
+```
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as _ from "lodash";
+
+// This is the main entrypoint by convention.
+// Everything it depends on is bundled by default.
+
+console.log(_.join(['a', 'b']))
